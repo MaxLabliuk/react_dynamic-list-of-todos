7d4cc1b7f371c95f471297f809758dc6253a4281
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2019-12-20 22:28:22 +0200

    Updates

---
+++ precache-manifest.96a609ae5243b30f7b94ff4524621aa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9138afabc2ea750dfedf4d8e2418d302",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "ec52befaeb68aaba52cb",
+    "url": "/react_dynamic-list-of-todos/static/css/main.d0f63111.chunk.css"
+  },
+  {
+    "revision": "55e58625568574069846",
+    "url": "/react_dynamic-list-of-todos/static/js/2.3811fce5.chunk.js"
+  },
+  {
+    "revision": "ec52befaeb68aaba52cb",
+    "url": "/react_dynamic-list-of-todos/static/js/main.0948c919.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
